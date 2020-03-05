812467442cb1c0870949cc4ec68912a524c2a3a4
jay_lac2000 <jayden.lacasse14@gmail.com>
2020-03-05 05:27:18 -0500

    what to do... hmm

--- src/jaylac2000/Staff/Main.php
+++ src/jaylac2000/Staff/Main.php
@@ -34,7 +34,7 @@ class Main extends PluginBase implements Listener
                     else
                         $sender->sendMessage("§b" . $smem . " : §cOffline");
 
-                } 
+                }
             }
             return false;
         }
