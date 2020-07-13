170228b287e915a9014c092472a1050af4582010
jay_lac2000 <jayzcraft.2000@gmail.com>
2020-07-12 21:58:08 -0400

    revert invis system for now
    gonna release the main part of the plugin again, than fix invis system than ill re-add/release

--- README.md
+++ README.md
@@ -1,28 +1,26 @@
 # StaffStatus
 
-This plugin will allow you to list your staff and their status weather it be them online or offline and now adds support for a invisible mode, read infomation on it below
+This plugin will allow you to list your staff and their status weather it be them online or offline
 
-# Commands: 
+# Commands:
 
 - /staff
-- /invisible
 
 # Permissions:
 
 - /staff : NO PERM = DEFAULT
-- /invisible : invis.use
 
-# Command Aliases: 
+# Command Aliases:
 
 - /staff : There is no alias to this command
-- /invisible : /invis
 
 # Config: 
 - Edit the config to add your self and your staff members
 - Also broadcast join / leave messages for invisible mode if you wish
 
-# COMING SOON: 
+# COMING SOON:
 
+- Invisable System
 - Support to show the staff members rank
 - Support to set the staff members status to offline while in invisible mode
 - Support for MySQL? To cross server staff / status? Will need help on this one...
@@ -35,10 +33,10 @@ Feel free to check out my server ApolloMCPE and test /staff for yourself (Does n
 IP: play.apollomcpe.xyz
 Port: 19132
 
-# Invisable mode
+# Invisable mode (BROKEN - WIP)
 
 - Does not show hand item
-- Does not show armour you are wearing 
+- Does not show armour you are wearing
 - Hides you from /list so players cannot see that you are online
 - Also has a perm "invis.see" to supply your Admins so they can see other players inside of invisible mode (Default op)
 
--- src/jaylac2000/StaffStatus/Main.php
+++ src/jaylac2000/StaffStatus/Main.php
@@ -10,17 +10,8 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\command\CommandSender;
 use pocketmine\scheduler\PluginTask;
 use pocketmine\utils\TextFormat as C;
-use pocketmine\network\mcpe\protocol\PlayerListPacket;
-use pocketmine\network\mcpe\protocol\types\PlayerListEntry;
-use pocketmine\network\mcpe\protocol\types\SkinData;
-use pocketmine\network\mcpe\protocol\types\SkinAdapterSingleton;
-
-use function array_search;
-use function in_array;
-use function strtolower;
 
 class Main extends PluginBase implements Listener {
-class Invisible extends PluginBase {
 
     private $list;
     public function onEnable()
@@ -49,77 +40,5 @@ class Invisible extends PluginBase {
             return false;
         }
         return false;
-    }
-}
-
-    public const PREFIX = C::LIGHT_BLUE . "Invisible " . C::DARK_GRAY . "> ". C::RESET;
-
-    public static $invisible = [];
-
-    public static $nametagg = [];
-
-    public $pk;
-
-    protected static $main;
-
-    public function onEnable(){
-        self::$main = $this;
-        $this->getServer()->getPluginManager()->registerEvents(new EventListener(), $this);
-        $this->getScheduler()->scheduleRepeatingTask(new InvisibleTask(), 20);
-        @mkdir($this->getDataFolder());
-        $this->saveDefaultConfig();
-    }
-
-    public static function getMain(): self{
-        return self::$main;
-    }
-
-    public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args): bool{
-        $name = $sender->getName();
-        switch(strtolower($cmd->getName())){
-		case "invisible":
-		case "invis":
-	        if(!$sender instanceof Player){
-                $sender->sendMessage(self::PREFIX . C::DARK_RED . "Please, use this command in-side the game...");
-                return false;
-	        }
-
-	        if(!$sender->hasPermission("invisible.use")){
-		        $sender->sendMessage(self::PREFIX . C::DARK_RED . "You do not have permission to use this command");
-                return false;
-	        }
-
-            if(!in_array($name, self::$invisible)){
-                self::$invisible[] = $name;
-		        $sender->sendMessage(self::PREFIX . C::GREEN . "You are now invisible.");
-		        $nameTag = $sender->getNameTag();
-		        self::$nametagg[$name] = $nameTag;
-		        $sender->setNameTag("§l§f[§bInvisible§f]§r $nameTag");
-                if($this->getConfig()->get("enable-leave") === true){
-                    $msg = $this->getConfig()->get("FakeLeave");
-                    $msg = str_replace("%name", "$name", $msg);
-                    $this->getServer()->broadcastMessage($msg);
-             	}
-            }else{
-                unset(self::$invisible[array_search($name, self::$invisible)]);
-                foreach($this->getServer()->getOnlinePlayers() as $players){
-                    $players->showPlayer($sender);
-                    $nameTag = self::$nametagg[$name];
-                    $sender->setNameTag("$nameTag");
-                }
-             $pk = new PlayerListPacket();
-             $pk->type = PlayerListPacket::TYPE_ADD;
-                 $pk->entries[] = PlayerListEntry::createAdditionEntry($sender->getUniqueId(), $sender->getId(), $sender->getDisplayName(), SkinAdapterSingleton::get()->toSkinData($sender->getSkin()), $sender->getXuid());
-             foreach($this->getServer()->getOnlinePlayers() as $p)
-             $p->sendDataPacket($pk);
-		        if($this->getConfig()->get("enable-join") === true){
-                        $msg = $this->getConfig()->get("FakeJoin");
-                        $msg = str_replace("%name", "$name", $msg);
-                        $this->getServer()->broadcastMessage($msg);
-		        }
-               	    $sender->sendMessage(self::PREFIX . C::DARK_RED . "You are no longer invisible.");
-            }
-        }
-        return true;
     }
 }
